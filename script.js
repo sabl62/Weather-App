3879f577344849fbf5f0617aa2f495317e72a318
sabal62 <sabalplayz.new@gmail.com>
2025-12-09 18:14:34 +0545

    why

---
+++ script.js
@@ -0,0 +1,32 @@
+let apiKey = "442bae3791eb40b2922134552250611";
+
+async function getWeather() {
+    let cityInput = document.getElementById('city-input');
+    let temperatureText = document.getElementById('temperature');
+    let dayOrNightText = document.getElementById('isDay');
+    let currentWeatherText = document.getElementById('current');
+    let city = cityInput.value.trim();
+    try {
+        let response = await fetch(
+            `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
+        );
+
+        let data = await response.json();
+        console.log(data);
+
+        currentWeatherText.innerText = '☁️ ' + data.current.condition.text;
+        temperatureText.innerText = '🌡️ ' + data.current.temp_c + ' deg.C';
+        if(data.current.is_day === 1){
+            dayOrNightText.innerText = '🕒 Currently day time';
+        }
+        else{
+            dayOrNightText.innerText = '🕒 Currently Night time';
+        }
+    } catch (error) {
+        console.error('Error fetching weather:', error);
+        currentWeatherText.innerText = 'Error loading weather';
+    }
+    currentWeatherText.style.display = "block";
+    temperatureText.style.display = "block";
+    dayOrNightText.style.display = "block";
+}
